fd121e67bac6e855f9d550f59b1c0acf62717c49
Sebastian Schlenkrich <sebastian.schlenkrich@frame-consult.de>
2024-12-10 07:26:25 +0100

    Use Julia 1.10.7

--- ParallelScenarioCalculation/docker/init.sh
+++ ParallelScenarioCalculation/docker/init.sh
@@ -8,14 +8,14 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get install -y \
 sudo apt-get clean
 rm -rf /var/lib/apt/lists/*
 
-# get Julia 1.10.4
-wget https://julialang-s3.julialang.org/bin/linux/x64/1.10/julia-1.10.4-linux-x86_64.tar.gz
+# get Julia 1.10.7
+wget https://julialang-s3.julialang.org/bin/linux/x64/1.10/julia-1.10.7-linux-x86_64.tar.gz
 
 # extract binary etc.
-tar zxvf julia-1.10.4-linux-x86_64.tar.gz
+tar zxvf julia-1.10.7-linux-x86_64.tar.gz
 
 # make Julia accessible
-sudo ln -s /home/celery/julia-1.10.4/bin/julia /usr/local/bin/
+sudo ln -s /home/celery/julia-1.10.7/bin/julia /usr/local/bin/
 
 git config --global core.autocrlf input
 
